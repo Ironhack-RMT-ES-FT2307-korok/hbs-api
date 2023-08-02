de96b6125aaf1943ebfce9fa8543004dfc939ed7
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-08-02 16:15:57 +0200

    done

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.set('view engine', 'hbs');
 // abajo indica al servidor que todas las vistas estarán ubicadas en la carpeta absoluta "views"
 app.set("views", __dirname + "/views/")
 
+const hbs = require('hbs');
+hbs.registerPartials(__dirname + "/views/partials")
+
+// inicializamos acceso al envoltorio de la API
+const DogApi = require('doggo-api-wrapper');
+const myDog = new DogApi();
+
 // Sección de busqueda de data
 const allLessons = require("./data/all-lessons.js")
 
@@ -64,6 +71,73 @@ app.get("/lessons/approved/:approvedType", (req, res, next) => {
   })
 
 })
+
+app.get("/perritos/aleatorio", (req, res, next) => {
+
+  myDog.getARandomDog()
+  .then((response) => {
+    console.log(response)
+    
+    res.render("dogs/random.hbs", {
+      imagen: response.message
+    })
+
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+
+
+})
+// 1. crear la ruta
+// 2. crear la vista
+// 3. renderizar la vista
+// 4... todo el resto
+
+app.get("/perritos/lista-de-razas", (req, res, next) => {
+
+  myDog.getListOfAllBreeds()
+  .then((response) => {
+    console.log(response.message)
+    let razas = [];
+    for (let key in response.message) {
+      // console.log(key)
+      razas.push(key)
+    }
+    console.log(razas)
+    // let razas = Object.keys(response.message)
+    // console.log(razas)
+    res.render("dogs/list.hbs", {
+      razas: razas
+    })
+
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+  
+})
+
+app.get("/perritos/por-raza/:raza", (req, res, next) => {
+
+  console.log(req.params.raza)
+  myDog.getAllDogsByBreed(req.params.raza)
+  .then((response) => {
+    console.log(response)
+    res.render("dogs/dogs-by-raza.hbs", {
+      dogs: response.message
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+  
+
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
